98de026e8bfc17183649866aa941b9631076a4b5
JacobBarthelmeh <jacob@wolfssl.com>
2014-05-19 16:59:14 -0600

    added some to server-psk.c for psk

--- server-psk.c
+++ server-psk.c
@@ -19,6 +19,7 @@
  */
 
 #include <cyassl/ssl.h> /* include cyassl security */
+#include <cyassl/options.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -34,11 +35,11 @@
 #define SA struct   sockaddr
 #define LISTENQ     1024
 #define SERV_PORT   11111
+//#define TLS_PSK_WITH_AES_128_CBC_SHA256 0XAE
 
 /* 
  * Fatal error detected, print out and exit. */
-void
-err_sys(const char *err, ...)
+void err_sys(const char *err, ...)
 {
     printf("Fatal error : %s\n", err);
     exit(1);
@@ -47,41 +48,44 @@ err_sys(const char *err, ...)
 /* 
  * Handles response to client.
  */
-void
-respond(CYASSL* ssl)
+void respond(CYASSL* ssl)
 {
     int  n;              /* length of string read */
     char buf[MAXLINE];   /* string read from client */
+    char response[22] = "I hear ya for shizzle";
     n = CyaSSL_read(ssl, buf, MAXLINE);
-    if(n > 0){
+    if (n > 0) {
         printf("%s\n", buf);
-        char response[22] = "I hear ya for shizzle";
-        if(CyaSSL_write(ssl, response, 22) > 22){
+        if (CyaSSL_write(ssl, response, 22) > 22) {
             err_sys("respond: write error");
         }
     }
-
-    if(n < 0){
+    if (n < 0) {
         err_sys("respond: read error");
     }
 }
 
 /*
- *Listen to the socket. 
+ *
  */
-void
-lstn(int listenfd, int backlog)
+unsigned int my_psk_server_cb(CYASSL* ssl, const char* identity, unsigned char* key,
+        unsigned int key_max_len)
 {
-    char* ptr;
-    if( (ptr = getenv("LISTENQ")) != NULL)
-        backlog = atoi(ptr);
-    
-    if(listen(listenfd, backlog) < 0)
-        err_sys("listen error");
+    (void)ssl;
+    (void)key_max_len;
+
+    if (strncmp(identity, "Client_identity", 15) != 0)
+        return 0;
+
+    key[0] = 26;
+    key[1] = 43;
+    key[2] = 60;
+    key[3] = 77;
+
+    return 4;
 }
 
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     int                 listenfd, connfd;
     struct sockaddr_in  cliaddr, servaddr;
@@ -91,7 +95,7 @@ main(int argc, char** argv)
     CyaSSL_Init();
     
     /* create ctx and configure certificates */
-    CYASSL_CTX* ctx;;
+    CYASSL_CTX* ctx;
     if((ctx = CyaSSL_CTX_new(CyaSSLv23_server_method())) == NULL)
         err_sys("CyaSSL_CTX_new error");
     if(CyaSSL_CTX_load_verify_locations(ctx, "certs/ca-cert.pem", 0) != 
@@ -105,11 +109,15 @@ main(int argc, char** argv)
         err_sys("Error loading certs/server-key.pem, please check the file");
    
     /* use psk suite for security */ 
-    CyaSSL_CTX_set_cipher_list(TLS_PSK_WITH_AES_128_CBC_SHA256);
+    CyaSSL_CTX_set_psk_server_callback(ctx, my_psk_server_cb);
+    //CyaSSL_CTX_set_psk_identity_hint(ctx, "cyassl server");
+    //if (CyaSSL_CTX_set_cipher_list(ctx, "PSK-AES128-CBC-SHA256") //"TLS_PSK_WITH_AES_128_CBC_SHA256")
+        //!= SSL_SUCCESS)
+        //err_sys("server can't set cipher list");
 
     /* find a socket */ 
     listenfd = socket(AF_INET, SOCK_STREAM, TCP);
-    if(listenfd < 0){
+    if (listenfd < 0) {
         err_sys("socket error");
     }
 
@@ -120,38 +128,38 @@ main(int argc, char** argv)
     servaddr.sin_port        = htons(SERV_PORT);
 
     /* bind to a socket */
-    if(bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+    if (bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
         err_sys("bind error");
     
     /* listen to the socket */   
-    lstn(listenfd, LISTENQ);
+    if (listen(listenfd, LISTENQ) < 0)
+        err_sys("listen error");
+
     
     /* main loop for accepting and responding to clients */
     for ( ; ; ) {
         clilen = sizeof(cliaddr);
         CYASSL* ssl;
         connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
-        if(connfd < 0){
-            if(errno != EINTR){
+        if (connfd < 0) {
+            if (errno != EINTR)
                 err_sys("accept error");
-            }
         }
-        else{
+        else {
             printf("Connection from %s, port %d\n",
-                inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
-                ntohs(cliaddr.sin_port));
+                   inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+                   ntohs(cliaddr.sin_port));
             /* create CYASSL object and respond */
-            if((ssl = CyaSSL_new(ctx)) == NULL)
+            if ((ssl = CyaSSL_new(ctx)) == NULL)
                 err_sys("CyaSSL_new error");
             CyaSSL_set_fd(ssl, connfd);
             respond(ssl);
             /* closes the connections after responding */
             CyaSSL_free(ssl);
-            if(close(connfd) == -1)
+            if (close(connfd) == -1)
                 err_sys("close error");
         }
     }
-
     /* free up memory used by cyassl */
     CyaSSL_CTX_free(ctx);
     CyaSSL_Cleanup();
