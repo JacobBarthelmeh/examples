983dbfd90d4ebf642afd9316701511ba27ceadf8
JacobBarthelmeh <jacob@wolfssl.com>
2014-05-19 14:33:05 -0600

    made changes to server-tcp.c to fit coding standards

--- server-tcp.c
+++ server-tcp.c
@@ -35,9 +35,9 @@
 #define SERV_PORT   11111
 
 /* 
- * Fatal error detected, print out and exit. */
-void
-err_sys(const char *err, ...)
+ * Fatal error detected, print out and exit. 
+ */
+void err_sys(const char *err, ...)
 {
     printf("Fatal error : %s\n", err);
     exit(1);
@@ -46,41 +46,24 @@ err_sys(const char *err, ...)
 /* 
  * Handles response to client.
  */
-void
-respond(int sockfd)
+void respond(int sockfd)
 {
     int  n;              /* length of string read */
     char buf[MAXLINE];   /* string read from client */
+    char response[22] = "I hear ya for shizzle";
     n = read(sockfd, buf, MAXLINE);
-    if(n > 0){
+    if (n > 0) {
         printf("%s\n", buf);
-        char response[22] = "I hear ya for shizzle";
-        if(write(sockfd, response, 22) > 22){
+        if (write(sockfd, response, 22) > 22) {
             err_sys("write error");
         }
     }
-
-    if(n < 0){
+    if (n < 0) {
         err_sys("respond: read error");
     }
 }
 
-/*
- *Listen to the socket. 
- */
-void
-lstn(int listenfd, int backlog)
-{
-    char* ptr;
-    if( (ptr = getenv("LISTENQ")) != NULL)
-        backlog = atoi(ptr);
-    
-    if(listen(listenfd, backlog) < 0)
-        err_sys("listen error");
-}
-
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     int                 listenfd, connfd;
     struct sockaddr_in  cliaddr, servaddr;
@@ -89,7 +72,7 @@ main(int argc, char** argv)
 
     /* find a socket */ 
     listenfd = socket(AF_INET, SOCK_STREAM, TCP);
-    if(listenfd < 0){
+    if (listenfd < 0) {
         err_sys("socket error");
     }
 
@@ -100,28 +83,28 @@ main(int argc, char** argv)
     servaddr.sin_port        = htons(SERV_PORT);
 
     /* bind to a socket */
-    if(bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+    if (bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
         err_sys("bind error");
     
     /* listen to the socket */   
-    lstn(listenfd, LISTENQ);
+    if (listen(listenfd, LISTENQ) < 0)
+        err_sys("listen error");
     
     /* main loop for accepting and responding to clients */
     for ( ; ; ) {
         clilen = sizeof(cliaddr);
         connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
-        if(connfd < 0){
-            if(errno != EINTR){
+        if (connfd < 0) {
+            if (errno != EINTR)
                 err_sys("accept error");
-            }
         }
-        else{
+        else {
             printf("Connection from %s, port %d\n",
-                inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
-                ntohs(cliaddr.sin_port));
+                   inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+                   ntohs(cliaddr.sin_port));
             respond(connfd);
             /* closes the connections after responding */
-            if(close(connfd) == -1)
+            if (close(connfd) == -1)
                 err_sys("close error");
         }
     }
